62ce886c5696af154afa53f7aaad880e0ca3aba5
Andrea Sansone <andreasansone35@gmail.com>
2023-10-11 14:28:38 +0200

    mobile menu done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,26 @@
 import { type AppType } from "next/dist/shared/lib/utils";
-
+import { Barlow, Fraunces } from "next/font/google";
 import "~/styles/globals.css";
 
+const barlow = Barlow({
+  subsets: ["latin"],
+  weight: "600",
+  variable: "--font-barlow",
+});
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  weight: ["700", "900"],
+  variable: "--font-fraunces",
+});
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <main
+      className={`${barlow.variable} ${fraunces.variable} w-full font-sans`}
+    >
+      <Component {...pageProps} />
+    </main>
+  );
 };
 
 export default MyApp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,27 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        Fm: "376px",
+      },
+      colors: {
+        "Fm-Soft-red": "hsl(7, 99%, 70%)",
+        "Fm-Yellow": "hsl(51, 100%, 49%)",
+        "Fm-Dark-desaturated-cyan": "hsl(167, 40%, 24%)",
+        "Fm-Dark-blue": "hsl(198, 62%, 26%)",
+        "Fm-Dark-moderate-cyan": "hsl(168, 34%, 41%)",
+        "Fm-Very-dark-desaturated-blue": "hsl(212, 27%, 19%)",
+        "Fm-Very-dark-grayish-blue": "hsl(213, 9%, 39%)",
+        "Fm-Dark-grayish-blue": "hsl(232, 10%, 55%)",
+        "Fm-Grayish-blue": "hsl(210, 4%, 67%)",
+        "Fm-White": "hsl(0, 0%, 100%)",
+      },
+      fontFamily: {
+        "Fm-barlow": ["var-(--font-barlow)"],
+        "Fm-fraunces": ["var(--font-fraunces)"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
